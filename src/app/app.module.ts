96adf5b333d10143aee947be5c739c31f4a928c3
Leo <danielesquivel00.lded@gmail.com>
2021-12-03 12:34:07 -0600

    [ADD] Estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookCardComponent } from './components/book-card/book-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { BookComponent } from './pages/book/book.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { ReportComponent } from './pages/report/report.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookCardComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    BookComponent,
+    AdminComponent,
+    CartComponent,
+    InventoryComponent,
+    ReportComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
