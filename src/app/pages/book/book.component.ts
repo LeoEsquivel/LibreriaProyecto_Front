75613f3057e56f9b47976ae066cb917a892fa680
Leo <danielesquivel00.lded@gmail.com>
2021-12-03 20:38:59 -0600

    [ADD] Connected book's page with API

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from 'src/app/models/book.model';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-book',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookComponent implements OnInit {
 
-  constructor() { }
+  public book: Book = {
+    _id: '',
+    isbn: '',
+    title: '',
+    author: {
+      _id: '',
+      name: ''
+    },
+    editorial: {
+      _id: '',
+      name: ''
+    },
+    price: 0,
+    amounth: 0,
+    imgUrl: ''
+  };
+
+  constructor(private _activateRoute: ActivatedRoute, private _httpService:HttpService) { }
 
   ngOnInit(): void {
+    this._activateRoute.params.subscribe(params =>{
+      const id: string = params['id'];
+      this._httpService.getBook(id).subscribe((book:Book)=>{
+        this.book = book;
+      })
+    })
   }
 
 }
