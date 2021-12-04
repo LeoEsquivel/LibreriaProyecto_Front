46d83b7c47a8af81a21ed70dc8b72cdd3a027535
Leo <danielesquivel00.lded@gmail.com>
2021-12-03 20:37:20 -0600

    [ADD] Login added

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public formLogin!: FormGroup;
+  errormessage!:string;
+  username!:string;
+  password!:string;
+
+  constructor(private _formBuilder: FormBuilder, private _httpService: HttpService) { }
+
+  
 
   ngOnInit(): void {
+    this.formLogin = this._formBuilder.group({
+        username: ['username', [Validators.required]],
+        password: ['password', [Validators.required]]
+    });
+  }
+
+  public send():void{
+    this._httpService.login(this.username, this.password).subscribe({
+      next: data=>{
+        console.log(data);
+      },
+      error: error => {
+        this.errormessage = error.message;
+        console.log('There was an error!', error)
+      }
+    })
+
   }
 
 }
