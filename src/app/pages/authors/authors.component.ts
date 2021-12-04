79a4f23737cbdf577cf3ca3d4b46eb1b81cfc354
Leo <danielesquivel00.lded@gmail.com>
2021-12-03 20:39:13 -0600

    [ADD] Connected author's page with API

--- src/app/pages/authors/authors.component.ts
+++ src/app/pages/authors/authors.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Author } from 'src/app/models/authors.model';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-authors',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorsComponent implements OnInit {
 
-  constructor() { }
+  public authors: Author[] = [];
+  
+  constructor(public _httpService: HttpService) { }
 
   ngOnInit(): void {
+    this._httpService.getAuthors().subscribe((authors:Author[])=>{
+      this.authors = authors;
+    })
   }
 
 }
