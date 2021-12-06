7025079b08f4734633f78092d7099b717c19b308
Leo <danielesquivel00.lded@gmail.com>
2021-12-05 18:53:50 -0600

    [ADD] Methods POST, DELETE and PATCH added

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -32,12 +32,20 @@ export class HttpService {
     return this._http.get<Author[]>(this.baseUrl + "/author");
   }
 
+  public getAuthor(id:string){
+    return this._http.get<Author>(this.baseUrl + "/author/"+id);
+  }
+
   public getAuthorsBooks(id:string){
     return this._http.get<Book[]>(this.baseUrl + "/book/" + id + "/auall");
   }
 
   public getEditorials(){
     return this._http.get<Editorial[]>(this.baseUrl + "/editorial");
+  }
+
+  public getEditorial(id:string){
+    return this._http.get<Editorial>(this.baseUrl+"/editorial/"+id);
   }
 
   public getEditorialsBook(id:string){
@@ -49,16 +57,62 @@ export class HttpService {
   }
 
   public addAuthor(name:string): Observable<any>{
-    const headers = { 'content-type': 'application/json'};  
-    const body = JSON.stringify(name);
-    return this._http.post(this.baseUrl+"/author", body, {'headers':headers});
+    const headers = { 'Content-Type': 'application/json'};  
+    var data = {
+      name: name
+    };
+    const body = JSON.stringify(data);
+    return this._http.post(this.baseUrl+"/author/", body, {headers: headers});
+  }
 
+  public addEditorial(name:string): Observable<any>{
+    const headers = { 'Content-Type': 'application/json'};  
+    var data = {
+      name: name
+    };
+    const body = JSON.stringify(data);
+    return this._http.post(this.baseUrl+"/editorial/", body, {headers: headers});
+  }
+
+  public addBook(book:any){
+    const headers = { 'Content-Type': 'application/json'};  
+    const body = JSON.stringify(book);
+    return this._http.post(this.baseUrl+"/book/", body, {headers: headers})
+  }
+
+  public deleteEditorial(id:string){
+    return this._http.delete(this.baseUrl+"/editorial/"+id);
+  }
+
+  public deleteAuthor(id:string){
+    return this._http.delete(this.baseUrl+"/author/"+id);
+  }
+
+  public deleteBook(id:string){
+    return this._http.delete(this.baseUrl+"/book/"+id);
+  }
+
+  public updateAuthor(id:string, author:Author){
+    const headers = { 'content-type': 'application/json'};
+    const body=JSON.stringify(author);
+    return this._http.patch(this.baseUrl+"/author/"+id, body, {headers: headers});
+  }
+
+  public updateEditorial(id:string, editorial:Editorial){
+    const headers = { 'content-type': 'application/json'};
+    const body=JSON.stringify(editorial);
+    return this._http.patch(this.baseUrl+"/editorial/"+id, body, {headers: headers});
+  }
+
+  public updateBook(id:string, book:Book){
+    const headers = { 'content-type': 'application/json'};
+    const body=JSON.stringify(book);
+    return this._http.patch(this.baseUrl+"/book/"+id, body, {headers: headers});
   }
 
   public login(username: string, password: string): Observable<any>{
       const headers = { 'content-type': 'application/json'};  
       const body=JSON.stringify({'username': username, 'password': password});
-      console.log(body)
       return this._http.post(this.baseUrl+"/auth/signin", body, {'headers':headers});
   }
 }
